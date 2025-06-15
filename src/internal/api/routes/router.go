ce4a024de00182c45f0df2e33fbe62165a3d0f13
Abrahan <amontealegre42@gmail.com>
2025-06-15 12:16:27 -0500

    add Swagger documentation and update user models with example fields

--- src/internal/api/routes/router.go
+++ src/internal/api/routes/router.go
@@ -18,6 +18,6 @@ func SetupV1(r *gin.Engine, db *gorm.DB) {
 		api.GET("/users", userHandler.GetAll)
 		api.POST("/register", userHandler.CreateUser)
 		api.POST("/login", userHandler.Login)
-		api.GET("/perfil/:name", userHandler.Perfil)
+		api.GET("/profile/:name", userHandler.Profile)
 	}
 }
--- src/internal/services/user_service.go
+++ src/internal/services/user_service.go
@@ -20,7 +20,6 @@ func (s *UserService) GetAllUsers() ([]models.User, error) {
 func (s *UserService) CreateUser(user *dtos.CreateUserDTO) (models.User, error) {
 	hashedPassword, err := utils.HashPassword(user.Password)
 	if err != nil {
-
 		return models.User{}, err
 	}
 	user.Password = hashedPassword
@@ -28,28 +27,24 @@ func (s *UserService) CreateUser(user *dtos.CreateUserDTO) (models.User, error) 
 }
 
 func (s *UserService) Login(user *dtos.LoginUser) (models.User, error) {
-
 	userData, err := s.Repo.FindByEmail(user)
-
 	if err != nil {
 		return models.User{}, errors.New("incorrect credentials")
 	}
 
 	IsAuthenticated := utils.ComparePassword(userData.Password, user.Password)
 	if !IsAuthenticated {
-
 		return models.User{}, errors.New("incorrect credentials")
 	}
 
 	return userData, nil
 }
 
-func (s *UserService) Perfil(name *string) (models.User, error) {
-
-	userPerfil, err := s.Repo.FindByName(name)
+func (s *UserService) Profile(name *string) (models.User, error) {
+	userProfile, err := s.Repo.FindByName(name)
 	if err != nil {
 		return models.User{}, errors.New("user not found")
 	}
 
-	return userPerfil, nil
+	return userProfile, nil
 }
